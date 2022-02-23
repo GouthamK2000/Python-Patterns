4ba3df49131cfbed831d56ec5a5397aa885255ee
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-02-23 06:18:12 +0530

    can print any qudrilateral

---
+++ p2-square.py
@@ -0,0 +1,7 @@
+r=int(input("Ente rthe no of rows: "))
+c=int(input("Enter the no. of columns: "))
+sym=input("Enter the symbol: ")
+for i in range(r):
+    for i in range(c):
+        print(sym+" ",end="")
+    print()
