030efe55136a901ba97c9dd3fd785dae89c7a1e5
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-02-25 07:29:57 +0530

    columnwise increase added

--- p3-squarerownumincrease.py
+++ p3-squarerownumincrease.py
@@ -4,3 +4,13 @@ for i in range(row):
     for j in range(col):
         print((i+1),end=" ")
     print()
+
+
+""" row=int(input("Enter the no. of rows: "))
+col=int(input("Enter the no. of columns: "))
+for i in range(row):
+    for j in range(col):
+        print((j+1),end=" ")               Here j is used in place of i, if we want to print  same elements columnwise
+    print()
+
+ """
