e08a182c4626fc02bcafdbd16beebc3a0bc4339a
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-03-01 04:48:13 +0530

    nested for loops for pyramids

--- p7-pyramidnumincrease.py
+++ p7-pyramidnumincrease.py
@@ -1,3 +1,12 @@
 h=int(input("Enter the height: "))
 for i in range(h):
-    print(" "*(h-1-i)+(str(i+1)+" ")*(i+1))
+   print(" "*(h-1-i)+(str(i+1)+" ")*(i+1))
+
+#Another using nested for loop
+
+# n=int(input("Enter the height of the pyramid: "))
+# for i in range(n):
+#     print(" "*(n-i-1),end=" ")
+#     for j in range(i+1):
+#         print("#",end=" ")
+#     print()
