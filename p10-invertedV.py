db93d661a3ce6af1205fee236fb158f49f6835a0
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-03-05 23:01:52 +0530

    Almost completed

--- p10-invertedV.py
+++ p10-invertedV.py
@@ -2,4 +2,5 @@ n=int(input("Enter n: "))
 for i in range(n):
     print(" "*(n-1-i)+" # ",end=" ")
     if i>1:
-        print(" "*(2*i-1)+"#",end=" ")
+        print(" "*(2*(i-1))+"#",end=" ")
+    print()
